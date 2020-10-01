fb5badc06f0a5a0cb00ac37865f974e4f8d8d13e
sj-onu <67582404+sj-onu@users.noreply.github.com>
2020-10-01 12:29:46 +0600

    hi

--- Donation/models.py
+++ Donation/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from Donor.models import DonorClass
 # Create your models here.
 class DonationClass(models.Model):
-    donor_id = models.ForeignKey(DonorClass, on_delete=models.SET_NULL, blank=True, null=True)
-    donation_id = models.IntegerField(unique=True, blank=True, null=True)
+    donor_name = models.ForeignKey(DonorClass, on_delete=models.SET_NULL, blank=True, null=True)
     amount = models.IntegerField(blank=True, null=True)
 
     def __int__(self):
-        return self.donation_id
+        return self.donor_name
